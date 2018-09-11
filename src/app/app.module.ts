263f135ba74e62446f10208915ec87daf36d786e
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-09-11 15:41:29 +1000

    added /users and /posts routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { PostsComponent } from './components/posts/posts.component';
 import { PostFormComponent } from './components/post-form/post-form.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { AppRoutingModule } from './/app-routing.module';
     NavbarComponent,
     PostsComponent,
     PostFormComponent,
+    HomeComponent,
   ],
   imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
   providers: [],
